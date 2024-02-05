8bb55dba15bac7e47428335b581cce213d39c29d
Bruno Delgado Herrero <delgado.hebru24@triana.salesianos.edu>
2024-02-05 12:29:12 +0100

    Ejercicio 1 casi

---
+++ EjerciciosUD5BrunoDelgado/src/ejercicio01/CrudNota.java
@@ -0,0 +1,71 @@
+package ejercicio01;
+
+import java.util.List;
+
+public class CrudNota {
+
+	private List<Nota> lista;
+
+	public CrudNota(List<Nota> lista) {
+		this.lista = lista;
+	}
+
+	public List<Nota> getLista() {
+		return lista;
+	}
+
+	public void setLista(List<Nota> lista) {
+		this.lista = lista;
+	}
+
+	@Override
+	public String toString() {
+		return "CrudNota [lista=" + lista + "]";
+	}
+	
+	public void imprimirNotas() {
+		System.out.println(lista);
+	}
+	
+	public void agregar(Nota n) {
+		
+		lista.add(n);
+		aumentarContador();
+	}
+	
+	public int aumentarContador() {
+		int contador=0;
+		contador++;
+		return contador;
+	}
+	
+	public void mostrarContador() {
+		System.out.println(aumentarContador());
+	}
+	
+	public void borrar (int id) {
+		if(buscarById(id) != null) {
+			lista.remove(buscarById (id));
+		}
+	}
+	
+	public Nota buscarById (int id) {
+		boolean encontrado=false;
+		int i=0;
+		
+		while(i<lista.size() && !encontrado) {
+			Nota deLista = lista.get(i);
+			if(deLista.getId() == id) {
+				encontrado = true;
+			} else {
+				i++;
+			}
+		}
+		if(encontrado) {
+			return lista.get(i);
+		} else {
+			return null;
+		}
+	}
+	
+}
---
+++ EjerciciosUD5BrunoDelgado/src/ejercicio04/Alumno.java
@@ -0,0 +1,15 @@
+package ejercicio04;
+
+public class Alumno {
+	private String nombre;
+	private String apellidos;
+	private int codAlum;
+	
+	public Alumno(String nombre, String apellidos, int codAlum) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.codAlum = codAlum;
+	}
+
+}
